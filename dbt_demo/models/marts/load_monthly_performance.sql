f26b9cd30eea624da4fb7004eda1d961a3e1d5e1
bohemianmoon5 <bohemianmoon5@gmail.com>
2026-03-04 15:42:22 +0900

    update file

--- dbt_demo/models/marts/load_monthly_performance.sql
+++ dbt_demo/models/marts/load_monthly_performance.sql
@@ -2,8 +2,8 @@ SELECT
 	  TO_CHAR(DATE_TRUNC('MONTH', c.START_DATE), 'YYYY-MM') AS MONTH
 	, SUM(p.COST)    AS TOTAL_COST
 	, SUM(p.REVENUE) AS TOTAL_REVENUE
-FROM ref {{ 'CAMPAIGN_PERFORMANCE' }} p
-JOIN ref {{ 'P_CAMPAIGNS' }} c
+FROM {{ ref('raw_campaign_performance') }} p
+JOIN {{ ref('raw_campaigns') }} c
   ON p.CAMPAIGN_ID = c.CAMPAIGN_ID
 GROUP BY 1
 ORDER BY 1
