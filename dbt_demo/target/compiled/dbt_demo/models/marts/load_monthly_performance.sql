b3eb2f3061ffa17dc1240386fc5ecf3d9665f562
bohemianmoon5 <dongchan1.seo@dongkuk.com>
2026-03-05 06:52:04 +0000

    commit

--- dbt_demo/target/compiled/dbt_demo/models/marts/load_monthly_performance.sql
+++ dbt_demo/target/compiled/dbt_demo/models/marts/load_monthly_performance.sql
@@ -2,8 +2,8 @@ SELECT
 	  TO_CHAR(DATE_TRUNC('MONTH', c.START_DATE), 'YYYY-MM') AS MONTH
 	, SUM(p.COST)    AS TOTAL_COST
 	, SUM(p.REVENUE) AS TOTAL_REVENUE
-FROM p_mart.p_mart_schema.raw_campaign_performance p
-JOIN p_mart.p_mart_schema.raw_campaigns c
+FROM p_source_db.p_source_schema.raw_campaign_performance p
+JOIN p_source_db.p_source_schema.raw_campaigns c
   ON p.CAMPAIGN_ID = c.CAMPAIGN_ID
 GROUP BY 1
 ORDER BY 1
